86b9fd6ef9cc55af463b2fa0515848e7cfde926a
Areez Visram <visraa7@mcmaster.ca>
2020-08-06 11:02:23 -0400

    done next game

--- clock.js
+++ clock.js
@@ -2,7 +2,12 @@ window.addEventListener("load", function () {
   function doDate() {
     var now = new Date();
 
-    document.getElementById("hours").innerHTML = now.getHours();
+    if(now.getHours().toString().length == 1) {
+        document.getElementById("hours").innerHTML = "0" + now.getHours();
+    } else {
+      document.getElementById("hours").innerHTML = now.getHours();
+    }
+
     if (now.getMinutes().toString().length == 1) {
       document.getElementById("minutes").innerHTML = "0" + now.getMinutes();
     } else {
@@ -24,15 +29,35 @@ window.addEventListener("load", function () {
     }
   }
 
-//   async function getNextGame() {
-//     const nextGameRequest = await fetch("https://statsapi.web.nhl.com/api/v1/teams/10?expand=team.schedule.next");
-//     let nextGameResponse = await nextGameRequest.json();
-//     let nextGameDate = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['date'];
-//     document.getElementById("next-game-date").innerHTML = "DATE: " + nextGameDate;
-//     console.log(nextGameResponse);
-//     let nextGameAway = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['away']['team']['name'];
-//   }
+  async function getNextGame() {
+    const nextGameRequest = await fetch("https://statsapi.web.nhl.com/api/v1/teams/10?expand=team.schedule.next");
+    let nextGameResponse = await nextGameRequest.json();
+    let nextGameDate = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['date'];
+    document.getElementById("next-game-date").innerHTML = "DATE: " + nextGameDate;
+    console.log(nextGameResponse);
+    let nextGameAway = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['away']['team']['name'];
+    document.getElementById("next-game-away").innerHTML = nextGameAway.toUpperCase();
+    let nextGameAwayWins = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['away']['leagueRecord']['wins'];
+    let nextGameAwayLosses = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['away']['leagueRecord']['losses'];
+    let nextGameAwayOT = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['away']['leagueRecord']['ot'];
+    let nextGameAwayRecord = nextGameAwayWins + "-" + nextGameAwayLosses + "-" + nextGameAwayOT;
+    document.getElementById("next-game-away-record").innerHTML = nextGameAwayRecord;
+
+    let nextGameHome = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['home']['team']['name'];
+    document.getElementById("next-game-home").innerHTML = nextGameHome.toUpperCase();
+    let nextGameHomeWins = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['home']['leagueRecord']['wins'];
+    let nextGameHomeLosses = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['home']['leagueRecord']['losses'];
+    let nextGameHomeOT = nextGameResponse['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]['teams']['home']['leagueRecord']['ot'];
+    let nextGameHomeRecord = nextGameHomeWins + "-" + nextGameHomeLosses + "-" + nextGameHomeOT;
+    document.getElementById("next-game-home-record").innerHTML = nextGameHomeRecord;
+
+    let nextGameAwayLogoSrc = "images/teams/" +  nextGameAway + ".png";
+    document.getElementById("next-away-team-logo").src = nextGameAwayLogoSrc;
+
+    let nextGameHomeLogoSrc = "images/teams/" +  nextGameHome + ".png";
+    document.getElementById("next-home-team-logo").src = nextGameHomeLogoSrc;
+  }
 
   setInterval(doDate, 1000);
-//   getNextGame();
+  getNextGame();
 });

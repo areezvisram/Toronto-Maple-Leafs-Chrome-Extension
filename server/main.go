7e21ba972d9ecced3427f0b61107f554f0627bef
Areez Visram <visraa7@mcmaster.ca>
2024-06-03 11:31:31 -0400

    Return game information as resposne from nhl-schedule endpoint

--- server/main.go
+++ server/main.go
@@ -1,47 +1,141 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func handleProxy(w http.ResponseWriter, r *http.Request) {
-	url := "https://api-web.nhle.com/v1/club-schedule/TOR/week/now"
-	log.Println("Request made for schedule")
+type Team struct {
+	PlaceName struct {
+		Default string `json:"default"`
+	} `json:"placeName"`
+	Logo string `json:"logo"`
+}
 
-	req, err := http.NewRequest(r.Method, url, nil)
+type Game struct {
+	GameDate string `json:"gameDate"`
+	AwayTeam Team   `json:"awayTeam"`
+	HomeTeam Team   `json:"homeTeam"`
+}
+
+type NextGameResponse struct {
+	Games []Game `json:"games"`
+}
+
+type FilteredGame struct {
+	GameDate     string `json:"gameDate"`
+	AwayTeam     string `json:"awayTeam"`
+	HomeTeam     string `json:"homeTeam"`
+	AwayTeamLogo string `json:"awayTeamLogo"`
+	HomeTeamLogo string `json:"homeTeamLogo"`
+}
+
+type FilteredResponse struct {
+	Games []FilteredGame `json:"games"`
+}
+
+type DefaultHttpClient struct{}
+
+func (c *DefaultHttpClient) Do(req *http.Request) (*http.Response, error) {
+	return http.DefaultClient.Do(req)
+}
+
+func createRequest(method, url string, header http.Header) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 
-	for k, v := range r.Header {
+	for k, v := range header {
 		req.Header[k] = v
 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	return req, nil
+}
+
+func copyResponse(w http.ResponseWriter, resp *http.Response) {
+	defer resp.Body.Close()
+
+	var reader io.Reader
+	if strings.Contains(resp.Header.Get("Content-Encoding"), "gzip") {
+		gzipReader, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer gzipReader.Close()
+		reader = gzipReader
+	} else {
+		reader = resp.Body
+	}
+
+	// Read the response body
+	body, err := io.ReadAll(reader)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	defer resp.Body.Close()
-
-	for k, v := range resp.Header {
-		w.Header()[k] = v
+	// Unmarshal the response body into the NextGameResponse struct
+	var nextGameResponse NextGameResponse
+	if err := json.Unmarshal(body, &nextGameResponse); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// Filter the data
+	var filteredResponse FilteredResponse
+	for _, game := range nextGameResponse.Games {
+		filteredGame := FilteredGame{
+			GameDate:     game.GameDate,
+			AwayTeam:     game.AwayTeam.PlaceName.Default,
+			HomeTeam:     game.HomeTeam.PlaceName.Default,
+			AwayTeamLogo: game.AwayTeam.Logo,
+			HomeTeamLogo: game.HomeTeam.Logo,
+		}
+		filteredResponse.Games = append(filteredResponse.Games, filteredGame)
+	}
 
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
+	// Marshal the filtered data back to JSON
+	filteredData, err := json.Marshal(filteredResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the content type to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the filtered JSON data to the response
+	w.Write(filteredData)
+}
+
+func handleProxy(url string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Request made for schedule")
+
+		req, err := createRequest(r.Method, url, r.Header)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		copyResponse(w, resp)
+	}
 }
 
 func main() {
-	http.HandleFunc("/api/nhl-schedule", handleProxy)
+	http.HandleFunc("/api/nhl-schedule", handleProxy("https://api-web.nhle.com/v1/club-schedule/EDM/week/now"))
 	log.Println("Starting proxy server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
